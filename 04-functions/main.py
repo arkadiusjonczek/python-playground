7815fd127040ecac5b169dcd6b0586982d6e2257
Arkadius Jonczek <arkadius.jonczek@sva.de>
2026-05-08 15:30:41 +0200

    feat: add function example

---
+++ 04-functions/main.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+    try:
+        name = input("Enter your name: ")
+        output =  hello(name)
+        print(output)
+    except ValueError as e:
+        print("Error using the input value: " + str(e))
+
+def hello(name):
+    if name == "":
+        raise ValueError("Expected a full name. Got an empty string.")
+    return f"Hello {name}"
+
+if __name__ == "__main__":
+    main()
