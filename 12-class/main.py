dd9a9ec5fedef06a28d07c6bb11292470a3f983c
Arkadius Jonczek <arkadius.jonczek@sva.de>
2026-05-12 16:30:07 +0200

    feat: add class example

--- 12-class/main.py
+++ 12-class/main.py
@@ -5,5 +5,15 @@ class Person:
   def greet(self):
     print("Hello, my name is " + self.name)
 
+class SpecialPerson(Person):
+  pass
+
+  def greet(self):
+    super().greet()
+    print("I am special person")
+
 p1 = Person("Emil")
 p1.greet()
+
+p2 = SpecialPerson("Mary")
+p2.greet()
